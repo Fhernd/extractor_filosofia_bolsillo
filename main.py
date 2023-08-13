cfa9e59e731177f59ad40197f91cecd4dddf7470
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-08-13 13:25:51 -0500

    Primera demo de acceso a la API de Spotify

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('Hola, este es el primer proyecto de Python')
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+# Configurar credenciales
+client_id = '1a3a71a5b9ad46a08c9ec452c037a747'
+client_secret = 'abc'
+
+# Autenticación
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+# Haz una solicitud a la API, por ejemplo, buscar un álbum
+results = sp.search(q='album:Abbey Road artist:The Beatles', type='album')
+albums = results['albums']['items']
+
+for album in albums:
+    print(album['name'], album['release_date'])
